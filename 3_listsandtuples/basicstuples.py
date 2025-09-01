39ea1d94ff442e16bfdf06ddc2e45f8415a5891b
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-01 19:55:37 +0530

    tuplebasics

---
+++ 3_listsandtuples/basicstuples.py
@@ -0,0 +1,21 @@
+# Basics of tuples
+#Tuple is nothing but a collection of elements which is ordered and unchangeable(immutable).
+#Tuples are defined by enclosing the elements in parentheses () 
+# whereas lists are defined by enclosing the elements in square brackets [].
+#Tuples can contain elements of different data types like int, str, float etc just like lists.
+
+tup = (2) # this alone number consiered as integer here and its shows type as int and same for other datatypes also
+print(tup)
+print(type(tup))
+# to make alone element as tuple we can end that element with an ,
+tuplee = (2,) # now this is considered as valid tuple eleemnt
+print(tuplee)
+print(type(tuplee))
+
+tuple = () #we can print empty tuple
+print(tuple)
+
+
+list = [] #we can print empty list as well
+print(list)
+print(type(list))
