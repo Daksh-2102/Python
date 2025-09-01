24cc060b31270bff85e3da2ae12ad756baea1cb1
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-01 20:12:40 +0530

    tuplefunctions

---
+++ 3_listsandtuples/tuplemethods.py
@@ -0,0 +1,9 @@
+#Some Methods of tuple
+
+tup = (2,3,4,2,2)
+#1. tuple.index(element) :- It returns the index of that particular element.
+print(tup.index(2)) #0(index value)
+print(tup.index(3)) #1
+
+#2. tuple.count(element) :- Counts the occurence of that element
+print(tup.count(2))
