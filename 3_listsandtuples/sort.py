d8f3ee939d792a69f26aba6e7dd1d18910691238
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-01 21:50:28 +0530

    count_grades

---
+++ 3_listsandtuples/sort.py
@@ -0,0 +1,11 @@
+#sorting of the alphabets of the list
+#we can .split after the input fxn to create  mutliple strings
+
+# alpha = str(input("Enter multiple alphabets : ")).split(",")
+alpha = ["y",'i','a','z ']
+print(type(alpha))
+print(alpha)
+
+alpha.sort()
+
+print(alpha)
