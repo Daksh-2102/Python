a4845d531c92465c35167fc3946ba061e01e2e33
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-03 23:46:53 +0530

    randomNumgenerator

---
+++ 4_Dictonaryandsets/randomnumbergenerator.py
@@ -0,0 +1,7 @@
+#Randome number generator using library
+
+#we can import the random library and using in built function random.randrange we can print random numbers
+
+import random
+print(random.randrange(1,100))
+
