ac086f9570882d2bb37571bff7a86ebf71029cc3
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-04 16:52:31 +0530

    in

---
+++ 3_listsandtuples/slicingandin.py
@@ -0,0 +1,11 @@
+#slicing and usage of in keyword
+#inkeyword :- is used to check weather that keyword is present or not 
+
+tuple = ('a','b','c')
+
+print(tuple[0 : 2])
+
+if 'a' in tuple:
+    print("Yes")
+else:
+    print("No")
