0226fe2010cf2a875fefe795e559f46cc0ce924b
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-04 16:21:32 +0530

    listloops

---
+++ 3_listsandtuples/listusingloops.py
@@ -0,0 +1,25 @@
+#Loops lists
+
+# for loop
+list = ['apple',18,99,'mango']
+
+print("Elements of list1 : ")
+for x in list:
+    print(x)
+
+list2 = [9,8,7]
+
+print("Elements of list2 are :")
+for i in range(len(list2)):
+    #print(i)
+
+    print(list2[i])
+
+list3 = ["hey","hi"]
+
+#while loop
+i=0
+while i < len(list3):
+    print(list3[i])
+    i+=1
+
