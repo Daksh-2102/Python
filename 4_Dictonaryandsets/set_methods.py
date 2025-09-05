10d52f042719dcc78361adbe5c56793f95ecebae
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-05 13:13:25 +0530

    setsmethods

--- 4_Dictonaryandsets/set_methods.py
+++ 4_Dictonaryandsets/set_methods.py
@@ -13,9 +13,10 @@ set1.remove("raghu")
 print(set1)
 
 #3. .clear() :- clears all the elements of the set and shows the length of set = 0
-set1.clear()
+#set1.clear()
 print(set1) # it will print the empty set i.e set()
 print(len(set1)) #0
 
 #4. .pop() :- pops out the random value
-#
+set1.pop()
+print(set1)
