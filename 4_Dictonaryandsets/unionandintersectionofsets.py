e28f25ff21888685281d686b758a1396b2938c6a
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-05 13:24:46 +0530

    UnionAndIntersection

---
+++ 4_Dictonaryandsets/unionandintersectionofsets.py
@@ -0,0 +1,12 @@
+#Union and Intersection in sets 
+#1. unions :- combining two sets and making a single set
+#2. Intersection :- combines the common values of both the sets 
+
+set1 = {1,2,3}
+set2 = {2,3,4}
+
+print(set1.union(set2)) # {1,2,3,4}
+
+print(set1.intersection(set2)) #{2,3}
+
+
