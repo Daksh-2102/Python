c9fe318a25019464c0ca556992ebafdd669ec6a7
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-05 13:40:04 +0530

    praticeques1_Dict

---
+++ 4_Dictonaryandsets/prat_Q1_dict.py
@@ -0,0 +1,12 @@
+# Store the word meaning in the dictonary
+
+word_meaning = {
+    "table" : ("a piece of furniture","lists of facts and figures"),
+    "cat"   : "a small animal"
+}
+
+print(word_meaning)
+
+print("using for loop : ")
+for x in word_meaning:
+    print(word_meaning[x])
