cb0fb96c6685da17399d6355b4e802c0513d97cb
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-05 13:45:20 +0530

    praticeques2_set

---
+++ 4_Dictonaryandsets/prac_Q2_set.py
@@ -0,0 +1,12 @@
+# calucating the no. of classrooms needed my student if each subject requires one class room.
+
+subjects = {
+    'python','java','c++','python','js','java','python','java','c++','c'
+}
+
+print(type(subjects))
+print(subjects)
+
+print(len(subjects)) 
+
+# hence we required only 5 classrooms for the students 
