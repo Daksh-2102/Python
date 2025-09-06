923241a09c4a104b9a70ee342b92d47febde2365
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-06 15:21:23 +0530

    counting_whileloops

---
+++ 5_loops/countingusingwhile.py
@@ -0,0 +1,11 @@
+#counting using while loops
+
+i = 1
+n = int(input("Enter n : "))
+
+print("counting : ")
+while i <= n:
+    print(i)
+    i+=1
+
+print("End")
---
+++ 5_loops/loopsbasics.py
@@ -0,0 +1,9 @@
+#loops are used for repitative work.
+
+#while loops
+
+count = 1
+while count<= 5:
+    print(1)
+    count+=1
+print(count)
