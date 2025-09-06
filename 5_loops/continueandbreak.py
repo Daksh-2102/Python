00d8f2acb20139fc5151c70d0ef4f6a4b7772f5b
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-06 19:30:23 +0530

    continueandbreak

---
+++ 5_loops/continueandbreak.py
@@ -0,0 +1,18 @@
+#continue and break statement
+
+i = 1
+while  i<=10:
+    if (i==3):
+        break # will stop theiteration there
+    print(i)
+    i+=1
+print("end\n\n")
+
+j = 10
+while j<=20:
+    if(j==13):
+        j+=1
+        continue
+    print(j)
+    j+=1
+
