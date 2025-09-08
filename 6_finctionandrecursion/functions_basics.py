a55f9026115ef3b2f2b5038ff57a396fd90e58f7
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-08 19:04:43 +0530

    functionsbasics

---
+++ 6_finctionandrecursion/functions_basics.py
@@ -0,0 +1,14 @@
+#Functios :- are the block of code that performs specific tasks.
+'''
+def func_name(parameter1,parameter2...):
+    #work
+    return value(if needed)
+func_name(argument1,argument2..)
+'''
+
+def basics():
+    print("Hello Functions!")
+    
+basics()
+basics()
+basics()
