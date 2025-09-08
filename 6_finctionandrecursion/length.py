1886015fe8e9a78c922c8425c665c4a4ebb1ad75
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-08 19:50:43 +0530

    length_of_list

---
+++ 6_finctionandrecursion/length.py
@@ -0,0 +1,14 @@
+#length of the list
+
+def length_of_list(list1):
+    print(len(list1))
+
+list1 = [1,2,3,4,5,'vasu','daksh']
+length_of_list(list1)
+
+#for multiple lists
+name = ['daksh','vsu','edvard']
+def list_len(list):
+    print(len(list))
+
+list_len(name)
