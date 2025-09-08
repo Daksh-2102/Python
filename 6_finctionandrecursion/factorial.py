ab14c84103885ff6e8bb4a462dffc46eb797b10f
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-08 20:09:50 +0530

    factorail

---
+++ 6_finctionandrecursion/factorial.py
@@ -0,0 +1,12 @@
+#factorial of n number
+
+def fact(n):
+    fact = 1
+    for i in range(1,n+1):
+        fact *=i
+    return fact
+
+n = int(input("Enter n : "))
+factorial = fact(n)
+
+print(f"factorial of {n} :",factorial)
