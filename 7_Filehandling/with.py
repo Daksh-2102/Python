83fa117ecf365139c408eed7719f0c4686ff9f7d
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-11 21:45:24 +0530

    write

---
+++ 7_Filehandling/with.py
@@ -0,0 +1,11 @@
+#with syntax
+
+with open("/home/daksh/Documents/pythoncodes/7_Filehandling/with.txt","r") as f:
+    data = f.read()
+    print(data)
+
+with open("/home/daksh/Documents/pythoncodes/7_Filehandling/with.txt","w")as t:
+    write_data = t.write("using with to overwrite the previous text\n")
+
+with open("/home/daksh/Documents/pythoncodes/7_Filehandling/with.txt","a")as p:
+    append_data = p.write("It will overwrite in the end")
