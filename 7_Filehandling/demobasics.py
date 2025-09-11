11c47a3e306aefc9d791ec83500c45e3094a7f06
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-11 18:33:16 +0530

    Updated

--- 7_Filehandling/demobasics.py
+++ 7_Filehandling/demobasics.py
@@ -8,6 +8,8 @@ f = open("/home/daksh/Documents/pythoncodes/7_Filehandling/demo.txt","r")
 
 # print(data) #reads all the data present there
 
+#note :- if we already read the whole data as i readed above, we can't read the lines again. It jzt print space
+
 line1 = f.readline() # reads line1
 print(line1)
 
