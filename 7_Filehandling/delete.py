27f167f412f5ff5928319d241b2be81abe8d03be
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-11 22:18:23 +0530

    delete

---
+++ 7_Filehandling/delete.py
@@ -0,0 +1,9 @@
+'''Deleting a file can take place using a module called os
+we need to import os
+os.remove(file_nane)
+'''
+
+import os
+os.remove("/home/daksh/Documents/pythoncodes/7_Filehandling/delete.txt")
+
+#therefore, the delete.txt files gets deleted
