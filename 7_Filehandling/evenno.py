55780df3e89013fc0efa8694f86dddd223eeec0f
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-11 23:56:14 +0530

    count_of_evenno.

--- 7_Filehandling/evenno.py
+++ 7_Filehandling/evenno.py
@@ -1,13 +1,13 @@
-# print the count of even numbers from the evenno.txt file
+#print the count of even no. from the evenno.txt file
 
 count = 0
 
 with open("/home/daksh/Documents/pythoncodes/7_Filehandling/evenno.txt",'r') as f:
     data = f.read()
 
+    #now split it into commas
     new = data.split(",")
-
     for i in new:
-        if(int(i) % 2 ==0):
+        if(int(i) % 2 == 0):
             count+=1
 print(count)
