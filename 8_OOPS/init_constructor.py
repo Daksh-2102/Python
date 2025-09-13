8d254287954763b055a600ee0af1edd5cb97ab95
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-13 14:58:56 +0530

    constructor_init

--- 8_OOPS/init_constructor.py
+++ 8_OOPS/init_constructor.py
@@ -1,12 +1,13 @@
 # __init__ construuctor
 
 class Student:
-    def __init__(self,fullname):
-        self.name = fullname
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
         print("Add Students")
 
-s1 = Student("Daksh")
-print(s1.name)
+s1 = Student("Daksh", 100)
+print(s1.name,s1.marks)
 
-s2 = Student("Vasu")
-print(s2.name)
+s2 = Student("Vasu", 99)
+print(s2.name,s2.marks)
