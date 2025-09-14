6b0bf8e50b3738981ff28eab1bb6e159345e0ff0
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-14 12:16:49 +0530

    private_attributes

---
+++ 8_OOPS/privateattr.py
@@ -0,0 +1,19 @@
+''' Priavate attributes are those attributes that are only accessable inside the class
+and not accessable outside the class'''
+
+class Person:
+
+    #we can create pvt attr using double underscore(__)
+    def __init__(self,name):
+        self.name = name
+    def __heloo(self):
+        print("Helooo gg")
+
+    def wlcm(self):
+        self.__heloo()
+
+p1 = Person("Daksh")
+# print(p1.name)
+
+# p1.__heloo()
+p1.wlcm()
