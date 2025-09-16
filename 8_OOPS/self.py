84d472b5cd1a508e0e4a7a7bbd396c4488bb0240
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-16 22:10:40 +0530

    self_parameter

--- 8_OOPS/self.py
+++ 8_OOPS/self.py
@@ -7,5 +7,10 @@ class Car:
     def func(self):
         print(f"The cars is {self.brand} and color is {self.color} ")
 
+#if we don't want to pass the full object i.e self we jzt need to make that method a static method
+    @staticmethod
+    def wlcm():
+        print("Wlcm to the showroom")
 car1 = Car()
 car1.func()
+car1.wlcm()
