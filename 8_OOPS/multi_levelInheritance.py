a04ff7dfb026a88b5bc43d839c9568250c104fa7
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-17 23:01:27 +0530

    Multilevel_Inheritance

--- 8_OOPS/multi_levelInheritance.py
+++ 8_OOPS/multi_levelInheritance.py
@@ -1,4 +1,4 @@
-#multiple inheritance
+#multi-level inheritance
 
 class Employee:
 
@@ -14,18 +14,22 @@ class Coder(Employee):
     def show_again(self):
         print(f"{self.name} is {self.language} developer in {self.company}")
 
-# multiple inheritance :- cant use mro method + coder inherits both the properties of Employee and Coder  
+# multilevel inheritance :- cant use mro method + coder inherits both the properties of Employee and Coder  
 class Cyber(Coder):
     cybersecuirty = "Penetration testing"
     def show_cyber(self):
         print(f"{self.name} who is a {self.language} developer is also an {self.cybersecuirty}")
 
 
-emp1 = Employee()
+emp1 = Employee() #This only print the language attr
 emp1.show()
 
 coder1 = Coder()
-coder1.show_again()
+emp1.show()
+coder1.show_again() #since it contains both it prints both except cyber one
 
-cyber1 = Cyber()
+
+cyber1 = Cyber() #it prints all attributes
+cyber1.show()
+cyber1.show_again()
 cyber1.show_cyber()
