5f54ab4ab21e5ce81808100aec2f37cd5686c143
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-26 19:20:11 +0530

    type_hints

--- 9_AdvancePython/3_type_hints.py
+++ 9_AdvancePython/3_type_hints.py
@@ -1,2 +1,15 @@
 from typing import List, Dict , Tuple
 
+num : List[int] = [1,2,3,45]
+
+name : Tuple[str,int] = ('Daksh' , 45 , 'vasu' , 78)
+
+dict : Dict[str,int] = {
+    'Godl' : 25,
+    'soul' : 0,
+    'OG' : 12
+}
+
+print(num)
+print(name)
+print(dict)
