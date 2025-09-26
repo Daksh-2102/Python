5d45c2bede092dcf94dd1dac6ffe7e63f704bd04
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-26 17:03:12 +0530

    walrusoperator

---
+++ 9_AdvancePython/1_walrusoperator.py
@@ -0,0 +1,8 @@
+#walrus operator --> ' := ' --> used to improve readibilty and reduces funx calling
+
+if (n := len('Daksh') ) == 5:
+    print(f"{n} is greater than five")
+else :
+    print(f"{n} is lesser than five ")
+
+
