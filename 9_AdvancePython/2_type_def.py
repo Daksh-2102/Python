fdb1ee078124636ca1f32b6359fbd6a1a365b643
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-26 17:13:40 +0530

    type_definition

---
+++ 9_AdvancePython/2_type_def.py
@@ -0,0 +1,22 @@
+#Type defition -> defining the type of that variable
+
+n : int  = 5 
+
+name : str = "daksh"
+
+if name.find('s'):
+    print("Present hai")
+else :
+    print("Absent hai")
+
+
+n = name.capitalize()
+print(n)
+
+#In func
+
+def sum(a : int , b : int) -> int :
+    sum = a+b
+    print(sum)
+
+sum(1,8)
