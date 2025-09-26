cf8e7328ef7ff9b345661cd226259f1e0849b335
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-26 19:39:06 +0530

    merged_dict

---
+++ 5_merged_dict.py
@@ -0,0 +1,16 @@
+#merged dict
+
+Table1 = {
+    'Soul' : 144,
+    'OG' : 130,
+    'TR' : 144
+}
+
+Table2 = {
+    'Godl' : 83,
+    'VS'   : 89
+}
+
+merged = Table1 | Table2
+
+print(f"Merged tables after semis : ",merged)
