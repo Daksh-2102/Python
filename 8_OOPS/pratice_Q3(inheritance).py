ff627955eed8092c75cdb1437b8942549bc35044
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-26 16:53:52 +0530

    praticeQ3(inheritance)

---
+++ 8_OOPS/pratice_Q3(inheritance).py
@@ -0,0 +1,11 @@
+'''Create a class "Employe" and add salary and increment properties to it.
++-
+
+After that write a methof 'salaryincrement' method with @property decorator with a setter 
+which changes the value of the increment based on the salary'''
+
+class Employee:
+    salary = 1000
+    increment = 20
+    
+
