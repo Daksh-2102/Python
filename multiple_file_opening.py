4d1c43891958218f66efb671ec12e19e4b1d774f
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-26 20:12:21 +0530

    opening2files

---
+++ multiple_file_opening.py
@@ -0,0 +1,16 @@
+#we can open multiple files by using with statement
+
+
+# Opening two files together using `with` and parentheses
+with (
+    open('/home/daksh/Documents/pythoncodes/file1.txt', 'r') as f1,
+    open('/home/daksh/Documents/pythoncodes/file2.txt', 'r') as f2
+):
+    # Read contents of both files
+    content1 = f1.read()
+    content2 = f2.read()
+
+print("File 1 content:")
+print(content1)
+print("\nFile 2 content:")
+print(content2)
