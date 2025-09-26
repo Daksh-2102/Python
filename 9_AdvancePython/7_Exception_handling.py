2a4875a890d8d08cb063f2deea9d4ab682b0409c
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-26 22:54:02 +0530

    tryandexcept

---
+++ 9_AdvancePython/7_Exception_handling.py
@@ -0,0 +1,8 @@
+#exception : we can use try and except for smooth running of code even if there is any error in it.
+'''like while making web page if web page doesn't loads it shows error as Page not loaded
+i.e an eg of try and except'''
+
+
+
+
+    
