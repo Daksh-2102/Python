36f1d3f729419cb9e78b4768680943917d32088a
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-27 18:40:07 +0530

    list_compression

--- 9_AdvancePython/14_list_compression.py
+++ 9_AdvancePython/14_list_compression.py
@@ -2,9 +2,14 @@
 
 list1 = [1,2,3,4,5]
 
-squarelist = []
+# squarelist = []
 
-for i in list1:
-    squarelist.append(i*i)
+# for i in list1:
+#     squarelist.append(i*i)
 
-print(squarelist) 
+# print(squarelist) 
+
+#this above work can be done in easy way using list compression
+
+squarelist1 = [i*i for i in list1]
+print(squarelist1)
