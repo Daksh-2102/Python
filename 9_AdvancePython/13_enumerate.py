6be055c855196df2c6eee5e3d7591d89e557ffbf
Daksh-2102 <dakshvasu21@gmail.com>
2025-09-28 21:06:54 +0530

    enumerate_func

--- 9_AdvancePython/13_enumerate.py
+++ 9_AdvancePython/13_enumerate.py
@@ -1,7 +1,7 @@
 #Enumerate is the built-in func used when we need both index value and item value simuntaneously
 
-l = [1,2,3,4,5]
+l = [1,2,3,4,5,6]
 
 for index , i in enumerate(l):
    print(f"index---> {index} : item ---> {i}")
- 
+
