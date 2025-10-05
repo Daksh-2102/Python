fce3d7bea8f48380b70d87e1545ebd7879dd8c69
Daksh-2102 <dakshvasu21@gmail.com>
2025-10-05 17:41:35 +0530

    Sets

--- 10_AdvancedPy_byPatrick/5_sets.py
+++ 10_AdvancedPy_byPatrick/5_sets.py
@@ -1,8 +1,16 @@
 #Sets are collection of data types which are unordered , mutable and can't allows duplicates
 
-set1 = {1,2,3,1,2}
+set1 = {1,2,3,1,2,4,5}
 print(type(set1))
 print(set1) #doesn't allows duplicates
 
 for x in set1:
     print(x)
+
+set2 = set()
+
+set2.add(1)
+print(set2)
+
+set1.remove(2) , print(set1) 
+set1.discard(1) , print(set1)
