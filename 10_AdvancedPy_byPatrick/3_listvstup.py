d91d912db5937ff70f981b9ce87275accb30a80a
Daksh-2102 <dakshvasu21@gmail.com>
2025-10-05 11:54:49 +0530

    listvstuple

---
+++ 10_AdvancedPy_byPatrick/3_listvstup.py
@@ -0,0 +1,13 @@
+#list vs tuple memory allocation
+import sys
+list1 = [1,2,3,4]
+tuple1 = (1,2,3,4)
+
+print(sys.getsizeof(list1),'bytes')
+print(sys.getsizeof(tuple1),'bytes')
+
+'''Clearly list contains more memory as compared to tuple '''
+
+#also list takes more time as compare to tuple when working with large data
+
+#So, tuple is more efficient than tuple
