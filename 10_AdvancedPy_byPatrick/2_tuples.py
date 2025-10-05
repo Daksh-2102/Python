b54e06f3ba0ce3400fa308551febf10401d0d6e9
Daksh-2102 <dakshvasu21@gmail.com>
2025-10-05 11:39:03 +0530

    tuples(ad_py)

--- 10_AdvancedPy_byPatrick/2_tuples.py
+++ 10_AdvancedPy_byPatrick/2_tuples.py
@@ -8,3 +8,12 @@ print(mytup)
 tup1 = (1) #this is consider as int type
 tup2 = (1,) #this is now consider as tuple 
 print(type(tup1))
+
+print(mytup[3])
+
+# mytup[0] = 10 # tuples doesnot allows item modification
+print(mytup)
+#to do so convert it into list make changes and then convert back into tuple.
+
+for i in range(len(mytup)):
+    print(mytup[i])
