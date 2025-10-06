2bfd23feafe419c28b0594f46dd1870682446052
Daksh-2102 <dakshvasu21@gmail.com>
2025-10-06 15:57:46 +0530

    strings(2)

--- 10_AdvancedPy_byPatrick/7_strings(2).py
+++ 10_AdvancedPy_byPatrick/7_strings(2).py
@@ -1,6 +1,6 @@
 #strings part 2
 from timeit import default_timer as T
-str1 = ['a']* 100000
+str1 = ['a']* 100
 # print(str1) #['a', 'a', 'a', 'a', 'a', 'a']
 
 #bad way
@@ -22,8 +22,11 @@ stop = T()
 print(stop - start)
 
  
-#String formating :- old method -> %s and .format()
+#String formating :- old method -> % like %s for str ,%d for int , %f for float etc and .format()
 # new method -> f string
 
+str = "my name is daksh and my age is"
+age = 19
 
+print(f"{str} {age}")
 
