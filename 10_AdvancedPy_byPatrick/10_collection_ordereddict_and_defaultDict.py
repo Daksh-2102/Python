09b6583663205441c3d630523bab757737387b29
Daksh-2102 <dakshvasu21@gmail.com>
2025-10-06 19:20:30 +0530

    OrderedDict_and_DefaultDict

--- 10_AdvancedPy_byPatrick/10_collection_ordereddict_and_defaultDict.py
+++ 10_AdvancedPy_byPatrick/10_collection_ordereddict_and_defaultDict.py
@@ -1,6 +1,7 @@
 #collections -->> OrderedDict and defaultdict
 
 from collections import OrderedDict
+from collections import defaultdict
 
 ordered_dict = {}
 
@@ -11,3 +12,15 @@ ordered_dict['language'] = 'Py'
 
 print(ordered_dict)
 
+
+##### default dict jzt returns default value i.e 0 incase of error
+
+
+default_dict = defaultdict(int)
+
+default_dict['name'] = 'vasu'
+default_dict['age'] = 19
+default_dict['rollno'] = 3
+default_dict['language'] = 'Py'
+
+print(default_dict['agee']) 
