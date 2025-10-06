f73d463ea1eb5d1c74d90fa64e87b7310b2bbda6
Daksh-2102 <dakshvasu21@gmail.com>
2025-10-06 15:02:53 +0530

    strings

--- 10_AdvancedPy_byPatrick/6_strings.py
+++ 10_AdvancedPy_byPatrick/6_strings.py
@@ -7,3 +7,7 @@ print(str[::1]) #prints the complete string
 
 #reversing the string
 print(str[::-1]) #prints the complete reversed string
+
+#concetnation
+greeting = "Hello"
+print(greeting + " " + str)
