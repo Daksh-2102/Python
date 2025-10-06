96f65f1d40b042b87e42e0941fe8a15cfc291ac9
Daksh-2102 <dakshvasu21@gmail.com>
2025-10-06 19:11:34 +0530

    OrderedDict

--- 10_AdvancedPy_byPatrick/10_collection_ordereddict_and_defaultDict.py
+++ 10_AdvancedPy_byPatrick/10_collection_ordereddict_and_defaultDict.py
@@ -1,5 +1,13 @@
 #collections -->> OrderedDict and defaultdict
 
+from collections import OrderedDict
 
+ordered_dict = {}
 
+ordered_dict['name'] = 'vasu'
+ordered_dict['age'] = 19
+ordered_dict['rollno'] = 3
+ordered_dict['language'] = 'Py'
+
+print(ordered_dict)
 
