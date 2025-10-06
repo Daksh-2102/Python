ddbe921a98cdff7c1a728fbeb91fb572299ce482
Daksh-2102 <dakshvasu21@gmail.com>
2025-10-06 15:46:11 +0530

    strings(2)

--- 10_AdvancedPy_byPatrick/7_strings(2).py
+++ 10_AdvancedPy_byPatrick/7_strings(2).py
@@ -1,4 +1,19 @@
 #strings part 2
+from timeit import default_timer as T
+str1 = ['a']*6
+print(str1) #['a', 'a', 'a', 'a', 'a', 'a']
 
-str1 = 'a'*6
-print(str1)
+#bad way
+# start = T()
+string = "" 
+for i in str1:
+    string+=i
+print(string)
+
+
+#good wa6y
+
+#join them :- if wants to join any str use join fxn
+print("".join(str1)) #aaaaaa
+
+
