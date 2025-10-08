66582a6f95bb954754561105e3579b65e84e6c75
Daksh-2102 <dakshvasu21@gmail.com>
2025-10-08 18:20:26 +0530

    Permutations_and_combinations

--- 10_AdvancedPy_byPatrick/13_perandcom.py
+++ 10_AdvancedPy_byPatrick/13_perandcom.py
@@ -6,3 +6,14 @@ a = (1,2,3)
 per = permutations(a,2) # 2--> SET OF 2
 
 print(list(per))
+
+
+#Combinations
+
+from itertools import combinations
+
+b = (4,5,6)
+
+comb = combinations(b,2)
+
+print(list(comb))
