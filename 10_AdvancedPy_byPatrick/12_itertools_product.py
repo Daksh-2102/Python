c0e56896f9b7e10ff00046f41690ed59f6cc6ec2
Daksh-2102 <dakshvasu21@gmail.com>
2025-10-08 16:45:51 +0530

    Product

--- 10_AdvancedPy_byPatrick/12_itertools_product.py
+++ 10_AdvancedPy_byPatrick/12_itertools_product.py
@@ -1,2 +1,10 @@
 #Itertools :- product, permutations, combination, accumulate, groupby, and infinite iterators.
 
+from itertools import product
+
+a = [1,2]
+b = [3,4]
+
+pro = product(a,b)
+
+print(list(pro))
